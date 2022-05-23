c28c9cbb0be68ed9b834b6532a2c2f91486a92aa
Rodrigo Madrona De Jesus <rodrigomadronadejesus@outlook.com>
2022-05-23 09:54:08 -0300

    Criação da Render do APP

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from "react";
+
+import Render from "./components/Render";
+
+
+export default class App extends Component {
+    render (){
+        return (
+            <Render/>
+        );
+    }
+}
---
+++ src/components/Render.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from "react";
+import {
+    SafeAreaView
+} from 'react-native';
+
+import Styles from '../styles/Render';
+
+
+export default class Render extends Component {
+    
+    render (){
+        return (
+            <SafeAreaView style={Styles.Container}>
+
+            </SafeAreaView>
+        );
+    }
+}
